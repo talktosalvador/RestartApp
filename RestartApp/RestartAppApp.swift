abda2e90783037e3f97445f5e24f2d7d72e5503a
Salvador Vilca <talktosalvador@gmail.com>
2023-07-15 11:45:44 -0400

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Salvador on 15/7/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
