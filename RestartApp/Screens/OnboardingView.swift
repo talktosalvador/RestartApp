12bdca6277f936b686a6d2aa9bfe0e6192c7b3fe
Salvador Vilca <talktosalvador@gmail.com>
2023-07-17 11:34:57 -0400

    added center

--- RestartApp/Screens/OnboardingView.swift
+++ RestartApp/Screens/OnboardingView.swift
@@ -39,6 +39,24 @@ struct OnboardingView: View {
                 
                 // MARK: - CENTER
                 
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } //: CENTER
+                
+                Spacer()
+                
                 // MARK: - FOOTER
             } //: ZSTACK
         } //: VSTACK
