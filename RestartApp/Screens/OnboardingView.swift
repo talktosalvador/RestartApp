f181ed75ab9b95cd4ee06efb267336a629de97fa
Salvador Vilca <talktosalvador@gmail.com>
2023-07-17 12:03:26 -0400

    added footer

--- RestartApp/Screens/OnboardingView.swift
+++ RestartApp/Screens/OnboardingView.swift
@@ -58,6 +58,61 @@ struct OnboardingView: View {
                 Spacer()
                 
                 // MARK: - FOOTER
+                
+                ZStack {
+                    // PARTS OF THE CUSTOM BUTTON
+                    
+                    // 1. BACKGROUND (STATIC)
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // 2. CALL-TO-ACTION (STATIC)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    // 3. CAPSULE (DYNAMIC WIDTH)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    // 4. CIRCLE (DRAGGABLE)
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                             isOnboardingViewActive = false
+                        }
+                        
+                        Spacer()
+                    } //: HSTACK
+                    
+                } //: FOOTER
+                .frame(height: 80, alignment: .center)
+                .padding()
             } //: ZSTACK
         } //: VSTACK
     }
