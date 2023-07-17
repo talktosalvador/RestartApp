0a379146bac131636d6aa73da6b1efee835f576c
Salvador Vilca <talktosalvador@gmail.com>
2023-07-17 11:34:25 -0400

    added header

--- RestartApp/Screens/OnboardingView.swift
+++ RestartApp/Screens/OnboardingView.swift
@@ -18,6 +18,25 @@ struct OnboardingView: View {
             VStack(spacing: 20) {
                 // MARK: - HEADER
                 
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    Text("Share")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's not how much we give but
+                    how much love we put into giving.
+                    """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                } //: HEADER
+                
                 // MARK: - CENTER
                 
                 // MARK: - FOOTER
