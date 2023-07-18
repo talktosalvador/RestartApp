5f214aa05dadec90eb79dec14a8a04fc55e8dfcf
Salvador Vilca <talktosalvador@gmail.com>
2023-07-18 17:23:03 -0400

    adjusted dimensions

--- RestartApp/Views/CircleGroupView.swift
+++ RestartApp/Views/CircleGroupView.swift
@@ -9,21 +9,22 @@ import SwiftUI
 
 struct CircleGroupView: View {
     // MARK: - PROPERTY
-    
+
     @State var ShapeColor: Color
     @State var ShapeOpacity: Double
-    
+
     // MARK: - BODY
+
     var body: some View {
         ZStack {
             Circle()
                 .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
-                .frame(width: 260, height: 260, alignment: .center)
-            
+
             Circle()
                 .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
-                .frame(width: 260, height: 260, alignment: .center)
         } //: ZSTACK
+        .frame(maxWidth: 500)
+        .padding(80)
     }
 }
 
